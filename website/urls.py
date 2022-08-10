f8dae38fc4fd6655981e7d0eb0cbc3d14e59df36
berkayaktunc <berkayaktunc@gmail.com>
2022-08-10 18:50:46 +0300

    url bind done

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home page"),
+    path('add', views.add, name="add"),
+    path('subtract.html', views.subtract, name="subtract"),
+    path('multiple.html', views.multiple, name="multiple"),
+    path('divide.html', views.divide, name="divide"),
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+
+def subtract(request):
+    return render(request, 'subtract.html', {})
+
+
+def add(request):
+    return render(request, 'add.html', {})
+
+
+def multiple(request):
+    return render(request, 'multiple.html', {})
+
+
+def divide(request):
+    return render(request, 'divide.html', {})
