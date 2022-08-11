759432fda213a7dab2d73f89e18edc0fa257c140
berkayaktunc <berkayaktunc@gmail.com>
2022-08-11 18:32:24 +0300

    small changes

--- website/urls.py
+++ website/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('add', views.add, name="add"),
-    path('subtract.html', views.subtract, name="subtract"),
-    path('multiple.html', views.multiple, name="multiple"),
-    path('divide.html', views.divide, name="divide"),
+    path('subtract', views.subtract, name="subtract"),
+    path('multiple', views.multiple, name="multiple"),
+    path('divide', views.divide, name="divide"),
 ]
